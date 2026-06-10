d120c02e8e75f73d02c0d2692fb13727af6565e1
Sebastian Gallego <sebasglha@gmail.com>
2026-06-10 17:40:20 -0500

    feat: small fixes

--- src/Application/DTOs/LoanDtos.cs
+++ src/Application/DTOs/LoanDtos.cs
@@ -4,6 +4,6 @@ namespace backend_library.Application.DTOs;
 public record CreateLoanRequestDto(int BookId, Guid UserId);
 
 // 2. Lo mismo para la respuesta si lo necesitas
-public record LoanResponseDto(int Id, int BookId, Guid UserId, string DueDate, bool IsExtended);
+public record LoanResponseDto(int Id, int BookId, Guid UserId, string DueDate, string? ReturnDate, bool IsExtended, bool IsReturned);
 
 public record ReturnLoanRequestDto(int LoanId);
--- src/Application/Services/ILoanService.cs
+++ src/Application/Services/ILoanService.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using backend_library.Application.DTOs;
 
@@ -5,6 +7,7 @@ namespace backend_library.Application.Services;
 
 public interface ILoanService
 {
+    Task<IEnumerable<LoanResponseDto>> GetLoansAsync(Guid? userId = null);
     Task<LoanResponseDto> CreateLoanAsync(CreateLoanRequestDto request);
     Task ProcessExpiredLoansAsync();
     Task<LoanResponseDto> RenewLoanAsync(int loanId);
